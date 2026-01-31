04c977db61275f6bdfdc34ebcb9b372994424e33
MavWolverine <316111+mavwolverine@users.noreply.github.com>
2026-01-30 22:32:11 -0800

    Add missing shift symbols to svg

--- keymap-drawer/update-keymap.sh
+++ keymap-drawer/update-keymap.sh
@@ -13,6 +13,15 @@ source "$SCRIPT_DIR/.venv/bin/activate"
 echo "Parsing keymap..."
 keymap parse -c 10 -z "$PROJECT_ROOT/config/totem.keymap" > "$SCRIPT_DIR/keymap-parsed.yaml"
 
+# Add shifted key representations
+sed -i '' \
+  -e "s|- /|- {t: /, s: '?'}|g" \
+  -e "s|- ','|- {t: ',', s: <}|g" \
+  -e "s|- \\.|- {t: ., s: '>'}|g" \
+  -e "s|, ;]|, {t: ;, s: ':'}]|g" \
+  -e "s|- '-'|- {t: '-', s: _}|g" \
+  "$SCRIPT_DIR/keymap-parsed.yaml"
+
 # Generate SVG
 echo "Generating SVG..."
 keymap -c "$SCRIPT_DIR/config.yaml" draw "$SCRIPT_DIR/keymap-parsed.yaml" -o "$PROJECT_ROOT/keymap.svg"
